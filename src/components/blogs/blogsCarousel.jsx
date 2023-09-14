9192c3c4163a7eeb254bf9f37759c9183a8511ad
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2023-09-14 16:15:47 +0100

    Commit 2 by Young

---
+++ src/components/blogs/blogsCarousel.jsx
@@ -0,0 +1,39 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+const BlogsCarousel = ({ carouselDetails }) => {
+    const renderSlides =  carouselDetails.map((carouselDetail) => (
+        <div key={carouselDetail.id} style={{
+            backgroundImage: `url(${carouselDetail.image})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            position : "relative",
+            width : "90%",
+            borderRadius : "20px",
+            minHeight : "368px",
+            maxHeight : "400px",
+            margin : "0 5% 57px 5%"
+          }} >
+            <div className="hero-text">
+                {carouselDetail.text}
+            </div>
+            <button className="btn black hero-btn">Read More</button>
+        </div>
+))
+    return (
+        <Carousel
+        showArrows={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={2500}
+        showStatus={false}
+        className="carousel-container"
+      >
+        {renderSlides}
+      </Carousel>
+      
+    )
+  }
+  
+  export default BlogsCarousel
