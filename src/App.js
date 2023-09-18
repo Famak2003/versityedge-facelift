4f608d42d29ba6cd257c24a94427ea801e1dc97d
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2023-09-18 10:32:54 +0100

    Accepted changes made to the app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/homeComponents/Home";
 import PredictChance from "./components/predictChanceComponents/PredictChance";
-<<<<<<< HEAD
 import Blogs from "./components/blogs/blogs";
 // import { Router } from "react-router-dom/cjs/react-router-dom";
-=======
 import ContactUs from "./components/contactUs/ContactUs";
 import ScrollToTop from "./components/ScrollToTop";
->>>>>>> dd0172068e9a9be7df316f43a31133f9ab6347d6
 
 export function App() {
   return (
@@ -18,7 +15,6 @@ export function App() {
       <ScrollToTop />
       <div className="App relative flex max-w-[150rem] flex-col gap-[2rem] font-nunito text-xl lmobile:gap-[3rem] md:gap-[5rem]">
         <Header />
-<<<<<<< HEAD
         <div className="content">
           <Switch>
             <Route exact path="/">
@@ -33,16 +29,6 @@ export function App() {
           </Switch>
           {/* <Home /> */}
         </div>
-=======
-        <main className="content">
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="/predictChance" element={<PredictChance />}></Route>
-            <Route path="/contactUs" element={<ContactUs />}></Route>
-          </Routes>
-        </main>
-
->>>>>>> dd0172068e9a9be7df316f43a31133f9ab6347d6
         <Footer />
       </div>
     </>
