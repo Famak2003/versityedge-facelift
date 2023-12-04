1413ff39ec8001655620caa83eb1430e24ce4ee7
ERNEST CHRIS <ernestchris6@gmail.com>
2023-12-04 16:25:11 +0100

    Password reset

--- src/pages/Signup/Page1.jsx
+++ src/pages/Signup/Page1.jsx
@@ -9,7 +9,7 @@ import { getNextSignupPage } from "../../redux/slice/authSlice";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import useFetch from '../useFetch/useFetch';
+
 
 const Page1 = () => {
 
--- src/pages/resetPassword/Password1.jsx
+++ src/pages/resetPassword/Password1.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { getNextResetPasswordPage } from "../../redux/slice/authSlice";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Password1 = () => {
   const dispatch = useDispatch();
 
+  const [phoneNumber, setPhoneNumber] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(getNextResetPasswordPage("OTP"));
+    axios.post('http://versityedge1.eastus.cloudapp.azure.com/v1/auth/request-otp',{
+      phone : phoneNumber
+    })
+    .then(() => {
+      toast('OTP Sent');
+      dispatch(getNextResetPasswordPage("OTP"));
+    })
+    .catch((err) => {
+      toast(err.response.data.message);
+      console.log("err", err.response.data.message);
+    })
   };
 
   return (
@@ -30,6 +46,7 @@ const Password1 = () => {
         <input
           type="tel"
           placeholder="Phone number"
+          onChange={(e) => setPhoneNumber(e.target.value)}
           required
           minLength={10}
           maxLength={11}
@@ -51,6 +68,7 @@ const Password1 = () => {
           </button>
         </div>
       </form>
+      <ToastContainer />
     </div>
   );
 };
